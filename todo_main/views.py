80e700f012121089f8d8e6a7d93cdd0a68a4b8ab
Joseph Jogi <vjosephjogi@gmail.com>
2025-07-29 12:53:12 +0530

    add data

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import HttpResponse,render
-
+from todos.models import Task
 def home(request):
-    return render(request,'home.html')
+    tasks_not=Task.objects.filter(is_completed=False).order_by('updated_at')
+    tasks=Task.objects.filter(is_completed=True).order_by('updated_at')
+    context={
+        'tasks':tasks,'tasks_not':tasks_not
+    }
+    return render(request,'home.html',context)
