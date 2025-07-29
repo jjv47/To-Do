6277be7113c193f50221ce4c658728b600319435
Joseph Jogi <vjosephjogi@gmail.com>
2025-07-29 23:48:04 +0530

    Final

--- todos/urls.py
+++ todos/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns=[
-    path('addTask/',views.addTask,name='addTask')
+    path('addTask/',views.addTask,name='addTask'),
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    path('mark_as_Undone/<int:pk>/',views.mark_as_Undone,name='mark_as_Undone'),
+    path('edit_task/<int:pk>',views.edit_task,name='edit_task'),
+    path('delete_task/<int:pk>',views.delete_task,name='delete_task'),
 ]
